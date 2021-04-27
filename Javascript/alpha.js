295e5299f7d91c83976dd31c85b35b193bc31b5e
Shantanu Gupta <50038300+Shantanugupta1118@users.noreply.github.com>
2021-04-28 01:20:57 +0530

    Add files via upload

---
+++ Javascript/alpha.js
@@ -0,0 +1,122 @@
+var textWrapper = document.querySelector('.button-color-change');
+
+
+function a_button() {
+	document.getElementById('apple').src="../image/apple.png";
+	document.getElementById('message').innerHTML="A for Apple";
+}
+
+function b_button() {
+	document.getElementById('apple').src="../image/banana-min.png";
+	document.getElementById('message').innerHTML="B for Banana";
+}
+
+function c_button() {
+	document.getElementById('apple').src="../image/cat.png";
+	document.getElementById('message').innerHTML="C for Cat";		
+}
+
+function d_button() {
+	document.getElementById('apple').src="../image/dog.png";
+	document.getElementById('message').innerHTML="D for Dog";		
+}
+
+function e_button() {
+	document.getElementById('apple').src="../image/elephant.png";
+	document.getElementById('message').innerHTML="E for Elephant";		
+}
+
+function f_button() {
+	document.getElementById('apple').src="../image/frog.png";
+	document.getElementById('message').innerHTML="F for Frog";		
+}
+
+function g_button() {
+	document.getElementById('apple').src="../image/giraffe.png";
+	document.getElementById('message').innerHTML="G for Giraff";		
+}
+
+function h_button() {
+	document.getElementById('apple').src="../image/horse.png";
+	document.getElementById('message').innerHTML="H for Horse";		
+}
+
+function i_button() {
+	document.getElementById('apple').src="../image/icrcream.png";
+	document.getElementById('message').innerHTML="I for Ice Cream";		
+}
+
+function j_button() {
+	document.getElementById('apple').src="../image/joker.png";
+	document.getElementById('message').innerHTML="J for Joker";		
+}
+
+function k_button() {
+	document.getElementById('apple').src="../image/kite.png";
+	document.getElementById('message').innerHTML="K for Kite";		
+}
+
+function l_button() {
+	document.getElementById('apple').src="../image/lion.png";
+	document.getElementById('message').innerHTML="L for Lion";		
+}
+
+function m_button() {
+	document.getElementById('apple').src="../image/monkey.png";
+	document.getElementById('message').innerHTML="M for Monkey";		
+}
+function n_button() {
+	document.getElementById('apple').src="../image/nest.png";
+	document.getElementById('message').innerHTML="N for Nest";		
+}
+function o_button() {
+	document.getElementById('apple').src="../image/orange.png";
+	document.getElementById('message').innerHTML="O for Orange";		
+}
+function p_button() {
+	document.getElementById('apple').src="../image/parrot.png";
+	document.getElementById('message').innerHTML="P for Parrot";		
+}
+function q_button() {
+	document.getElementById('apple').src="../image/queen.png";
+	document.getElementById('message').innerHTML="Q for Queen";		
+}
+function r_button() {
+	document.getElementById('apple').src="../image/rat.png";
+	document.getElementById('message').innerHTML="R for Rat";		
+}
+function s_button() {
+	document.getElementById('apple').src="../image/ship.png";
+	document.getElementById('message').innerHTML="S for Ship";		
+}
+function t_button() {
+	document.getElementById('apple').src="../image/teacher.png";
+	document.getElementById('message').innerHTML="T for Teacher";		
+}
+function u_button() {
+	document.getElementById('apple').src="../image/umbrella.png";
+	document.getElementById('message').innerHTML="U for Umbrella";		
+}
+function v_button() {
+	document.getElementById('apple').src="../image/van.png";
+	document.getElementById('message').innerHTML="V for Van";		
+}
+function w_button() {
+	document.getElementById('apple').src="../image/whale.png";
+	document.getElementById('message').innerHTML="W for Whale";		
+}
+function x_button() {
+	document.getElementById('apple').src="../image/xray.png";
+	document.getElementById('message').innerHTML="X for X-Ray";		
+}
+function y_button() {
+	document.getElementById('apple').src="../image/yak.png";
+	document.getElementById('message').innerHTML="Y for YAK";		
+}
+function z_button() {
+	document.getElementById('apple').src="../image/zebra.png";
+	document.getElementById('message').innerHTML="Z for Zebra";		
+}
+
+
+
